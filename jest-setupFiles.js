e0b0e113fc235a6c16d29c742b25f54becf8ab27
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2026-01-22 21:25:55 +0200

    add jsPDF polyfills and include it in esModules list to support jspdf v4 in jest configuration

--- jest-setupFiles.js
+++ jest-setupFiles.js
@@ -1,1 +1,9 @@
 import 'regenerator-runtime/runtime';
+import {
+  TextEncoder,
+  TextDecoder,
+} from 'util';
+
+// Polyfills for jspdf
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
