8a65c86fbf62e6b3423b505183de7432d3a80d35
Zak Burke <zburke@ebsco.com>
2026-08-28 15:13:14 -0400

    Windows compat

--- stripes-jest.js
+++ stripes-jest.js
@@ -12,7 +12,7 @@ import { fileURLToPath } from 'node:url';
 
 const jestPackage = fileURLToPath(import.meta.resolve('jest/package.json'));
 const jest = join(dirname(jestPackage), 'bin', 'jest.js');
-const result = spawnSync(jest, process.argv.slice(2), { stdio: 'inherit' });
+const result = spawnSync(process.execPath, [jest, ...process.argv.slice(2)], { stdio: 'inherit' });
 
 if (result.error) {
   throw result.error;
